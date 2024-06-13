7ed1d34a69721ce169a23def5af657f5e6f9a079
Pavel02022022 <98917344+Pavel02022022@users.noreply.github.com>
2024-06-13 11:19:50 +0300

    sprint_6

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,35 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getKittens()).thenReturn(3);
+        int kittens = lion.getKittens();
+        assertEquals(3, kittens);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        Lion lion = new Lion("Самка", feline);
+        List<String> eat = lion.getFood();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), eat);
+    }
+
+}
